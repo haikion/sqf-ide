707fc004d2f7260d779877a8f2cc3bc96b88e127
Niko Häikiö <niko.haikio@gmail.com>
2016-04-05 13:44:58 +0300

    Features, producs, Config->config and more... /__\

---
+++ org.xtext.arma.config.tests/src/org/xtext/arma/config/tests/XtextUtilsTests.java
@@ -0,0 +1,37 @@
+package org.xtext.arma.config.tests;
+
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipselabs.xtext.utils.unittesting.XtextRunner2;
+import org.eclipselabs.xtext.utils.unittesting.XtextTest;
+import org.xtext.arma.ConfigInjectorProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(XtextRunner2.class)
+@InjectWith(ConfigInjectorProvider.class)
+public class XtextUtilsTests extends XtextTest
+{
+    public XtextUtilsTests()
+    {
+        super("ConfigFiles");
+    }
+    
+    @Test
+    public void emptyClass() {
+        testParserRule("class test {}", "Class");
+    }
+    
+    @Test
+    public void classInsideClass() {
+        testParserRule("class test {"
+                + "    class test2 {};"
+                + "}", "Class");
+    }
+    
+    @Test
+    public void classWithParameter() {
+        testParserRule("class test {"
+                + "tag = \"life\";"
+                + "}", "Class");
+    }
+}
