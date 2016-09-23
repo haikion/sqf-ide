1a30716b07d505c3e196417932c30ef4b8b7a124
Niko Häikiö <niko.haikio@gmail.com>
2016-09-24 02:06:48 +0300

    Minor fixes

--- org.arma.side.ui/src/org/arma/side/Activator.java
+++ org.arma.side.ui/src/org/arma/side/Activator.java
@@ -15,7 +15,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "SQF-IDE"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.arma.side.ui"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
--- org.arma.side.ui/src/org/arma/side/wizards/ImportMissionPage.java
+++ org.arma.side.ui/src/org/arma/side/wizards/ImportMissionPage.java
@@ -140,34 +140,4 @@ public class ImportMissionPage extends WizardPage {
             missionText_.setText(missionLocation_.getAbsolutePath());
         }
     }
-    
-    /*
-    /* (non-Javadoc)
-     * @see org.eclipse.ui.dialogs.WizardNewFileCreationPage#createAdvancedControls(org.eclipse.swt.widgets.Composite)
-        
-    protected void createAdvancedControls(Composite parent) {
-        Composite fileSelectionArea = new Composite(parent, SWT.NONE);
-        GridData fileSelectionData = new GridData(GridData.GRAB_HORIZONTAL
-                | GridData.FILL_HORIZONTAL);
-        fileSelectionArea.setLayoutData(fileSelectionData);
-
-        GridLayout fileSelectionLayout = new GridLayout();
-        fileSelectionLayout.numColumns = 3;
-        fileSelectionLayout.makeColumnsEqualWidth = false;
-        fileSelectionLayout.marginWidth = 0;
-        fileSelectionLayout.marginHeight = 0;
-        fileSelectionArea.setLayout(fileSelectionLayout);
-        
-        editor = new FileFieldEditor("fileSelect","Select Mission Directory: ",fileSelectionArea);
-        editor.getTextControl(fileSelectionArea).addModifyListener(new ModifyListener(){
-            public void modifyText(ModifyEvent e) {
-                IPath path = new Path(ImportMissionPage.this.editor.getStringValue());
-                setFileName(path.lastSegment());
-            }
-        });
-        String[] extensions = new String[] { "*.*" }; //NON-NLS-1
-        editor.setFileExtensions(extensions);
-        fileSelectionArea.moveAbove(null);
-    }
-    */
 }
