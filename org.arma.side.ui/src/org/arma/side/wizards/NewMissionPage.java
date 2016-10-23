5dd5dd3f4618b918f3d3f7fed36ed33a85019abe
Niko Häikiö <niko.haikio@gmail.com>
2016-10-23 23:40:19 +0300

    Style changes

--- org.arma.side.ui/src/org/arma/side/wizards/NewMissionPage.java
+++ org.arma.side.ui/src/org/arma/side/wizards/NewMissionPage.java
@@ -15,8 +15,8 @@ import org.eclipse.swt.widgets.Text;
  * OR with the extension that matches the expected one (mpe).
  */
 
-public class NewMissionPage extends WizardPage {
-	//private Text containerText;
+public class NewMissionPage extends WizardPage
+{
 	private Text nameText;
 	private ISelection selection;
 
@@ -25,7 +25,8 @@ public class NewMissionPage extends WizardPage {
 	 * 
 	 * @param pageName
 	 */
-	public NewMissionPage(ISelection selection) {
+	public NewMissionPage(ISelection selection)
+	{
 		super("wizardPage");
 		setTitle("New arma 3 mission");
 		setDescription("This wizard creates a new Arma 3 mission.");
@@ -35,7 +36,8 @@ public class NewMissionPage extends WizardPage {
 	/**
 	 * @see IDialogPage#createControl(Composite)
 	 */
-	public void createControl(Composite parent) {
+	public void createControl(Composite parent)
+	{
 		Composite container = new Composite(parent, SWT.NULL);
 		GridLayout layout = new GridLayout();
 		container.setLayout(layout);
@@ -45,35 +47,6 @@ public class NewMissionPage extends WizardPage {
 		label.setText("&Mission Name:");
 		nameText = new Text(container, SWT.NULL);
 
-		//containerText = new Text(container, SWT.BORDER | SWT.SINGLE);
-		//GridData gd = new GridData(GridData.FILL_HORIZONTAL);
-		//containerText.setLayoutData(gd);
-		//containerText.addModifyListener(new ModifyListener() {
-		//	public void modifyText(ModifyEvent e) {
-		//		dialogChanged();
-		//	}
-		//});
-
-		/*
-		Button button = new Button(container, SWT.PUSH);
-		button.setText("Browse...");
-		button.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				handleBrowse();
-			}
-		});
-		label = new Label(container, SWT.NULL);
-		label.setText("&File name:");
-
-		fileText = new Text(container, SWT.BORDER | SWT.SINGLE);
-		gd = new GridData(GridData.FILL_HORIZONTAL);
-		fileText.setLayoutData(gd);
-		fileText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				dialogChanged();
-			}
-		});
-		*/
 		initialize();
 		dialogChanged();
 		setControl(container);
@@ -83,70 +56,25 @@ public class NewMissionPage extends WizardPage {
 	 * Tests if the current workbench selection is a suitable container to use.
 	 */
 
-	private void initialize() {
+	private void initialize()
+	{
 		if (selection != null && selection.isEmpty() == false
 				&& selection instanceof IStructuredSelection) {
 			IStructuredSelection ssel = (IStructuredSelection) selection;
 			if (ssel.size() > 1)
 				return;
-			/*
-			Object obj = ssel.getFirstElement();
-			if (obj instanceof IResource) {
-				IContainer container;
-				if (obj instanceof IContainer)
-					container = (IContainer) obj;
-				else
-					container = ((IResource) obj).getParent();
-				//containerText.setText(container.getFullPath().toString());
-			}*/
 		}
 		nameText.setText("New Mission");
 	}
 
 	/**
-	 * Uses the standard container selection dialog to choose the new value for
-	 * the container field.
-	 */
-
-	/*
-	private void handleBrowse() {
-		ContainerSelectionDialog dialog = new ContainerSelectionDialog(
-				getShell(), ResourcesPlugin.getWorkspace().getRoot(), false,
-				"Select new file container");
-		if (dialog.open() == ContainerSelectionDialog.OK) {
-			Object[] result = dialog.getResult();
-			if (result.length == 1) {
-				containerText.setText(((Path) result[0]).toString());
-			}
-		}
-
-	}
-	*/
-
-	/**
 	 * Ensures that both text fields are set.
 	 */
 
-	private void dialogChanged() {
-		//IResource container = ResourcesPlugin.getWorkspace().getRoot()
-		//		.findMember(new Path(getContainerName()));
+	private void dialogChanged()
+	{
 		String missionName = getMissionName();
 
-		/*
-		if (getContainerName().length() == 0) {
-			updateStatus("File container must be specified");
-			return;
-		}
-		if (container == null
-				|| (container.getType() & (IResource.PROJECT | IResource.FOLDER)) == 0) {
-			updateStatus("File container must exist");
-			return;
-		}
-		if (!container.isAccessible()) {
-			updateStatus("Project must be writable");
-			return;
-		}
-		*/
 		if (missionName.length() == 0) {
 			updateStatus("File name must be specified");
 			return;
